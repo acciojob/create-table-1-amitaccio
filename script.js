5fbff7f430f016eb25e3962010e0c5e3b042b873
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-27 20:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
+    const table = document.getElementById("sampleTable");
+	// create and insert the table at desired position.
+	var newRow = table.insertRow(0);
+	// insert cell at 0 and 1 index places.
+   var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+	 cell1.innerText = "New Cell1";
+  cell2.innerText = "New Cell2";
   
   
 }
